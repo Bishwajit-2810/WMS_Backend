7cbd4ee920fe81b988cd2d2ad0f6ac315b22e7ac
Bishwajit_2810 <bishwajit2810@gmail.com>
2025-03-30 23:12:15 +0600

    Port Fixed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 require('dotenv').config()
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 // MongoDB Connection
 mongoose.connect(process.env.mongoUrl, {});
