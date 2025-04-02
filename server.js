ac86cd9f1cc6d6b692ee245f6d53cc476d7fbc09
Bishwajit_2810 <bishwajit2810@gmail.com>
2025-04-02 11:54:24 +0600

    AI API Post

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get("/api/data/latest", async (req, res) => {
 });
 
 // generate()
-app.get('/api/content', async (req, res) => {
+app.post('/api/content', async (req, res) => {
     try {
         const prompt = req.body.prompt
         const result = await generate(prompt + "in short and in points")
